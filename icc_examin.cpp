60361bbd2a43f0710814cc003f22b776f909704a
Kai-Uwe Behrmann <ku.b@gmx.de>
2010-05-03 13:25:12 +0200

    * [core]: fix buffer overflow

--- icc_examin.cpp
+++ icc_examin.cpp
@@ -861,7 +861,7 @@ ICCexamin::setzeFensterTitel()
       snprintf(t, t_len, "ICC Examin: %s - %s", title,
               _("Gamut View"));
 	  if(strlen(title) > t_len)
-	    sprintf( &t[t_len], "..." );
+	    sprintf( &t[t_len-4], "..." );
     } else
       snprintf(t, t_len, "ICC Examin: - %s", _("Gamut View"));
 
