aaffd1dbfcf57464b88831749a1e0a179c96d645
Kai-Uwe Behrmann <ku.b@gmx.de>
2011-01-07 15:28:50 +0100

    * [conf]: sync with Oyranos

--- configure_tests.sh
+++ configure_tests.sh
@@ -434,12 +434,59 @@ if [ -n "$LCMS2" ] && [ $LCMS2 -gt 0 ]; then
 fi
 
 if [ -n "$LRAW" ] && [ $LRAW -gt 0 ]; then
-  name="raw"
-  libname=$name
+  name="libraw"
+  libname=raw
   minversion=0.7
   url="http://www.libraw.org"
   TESTER=$LRAW
   ID=LRAW
+
+  ID_H="$ID"_H
+  ID_LIBS="$ID"_LIBS
+  HAVE_LIB=0
+  version=`pkg-config --modversion $name`
+  pkg-config  --atleast-version=$minversion $name
+  if [ $? = 0 ]; then
+    echo "#define HAVE_$ID 1" >> $CONF_H
+    l=$libname
+    rm -f tests/libtest$EXEC_END
+    $CXX $CXXFLAGS -I$includedir $ROOT_DIR/tests/libraw_test.cxx $LDFLAGS -L$libdir -shared `pkg-config --cflags --libs $name` -o tests/libtest 2>>$CONF_LOG
+    if [ -f tests/libtest$EXEC_END ]; then
+      HAVE_LIB=1
+      echo "#define HAVE_$ID 1" >> $CONF_H
+      echo "$ID = 1" >> $CONF
+      echo "$ID_H = `pkg-config --cflags $name | sed \"$STRIPOPT\"`" >> $CONF
+      echo "$ID_LIBS = `pkg-config --libs $name | sed \"$STRIPOPT\"`" >> $CONF
+      rm tests/libtest$EXEC_END
+    fi
+  fi
+  if [ $HAVE_LIB -eq 1 ]; then
+    if [ "$version" != "" ]; then
+      echo_="$name $version           detected"; echo "$echo_" >> $CONF_LOG; test -n "$ECHO" && $ECHO "$echo_"
+    else
+      echo_="$name                  detected"; echo "$echo_" >> $CONF_LOG; test -n "$ECHO" && $ECHO "$echo_"
+    fi
+  else
+    echo_="command was: $CXX $CXXFLAGS -I$includedir $ROOT_DIR/tests/libraw_test.cxx $LDFLAGS -L$libdir -shared -lraw -o tests/libtest"; echo "$echo_" >> $CONF_LOG; test -n "$ECHO" && $ECHO "$echo_"
+    $CXX $CXXFLAGS -I$includedir $ROOT_DIR/tests/libraw_test.cxx $LDFLAGS -L$libdir -shared `pkg-config --cflags --libs $name` -o tests/libtest
+    if [ $TESTER -eq 1 ]; then
+      echo_="!!! ERROR: no or too old $name found, !!!"; echo "$echo_" >> $CONF_LOG; test -n "$ECHO" && $ECHO "$echo_"
+      ERROR=1
+    else
+      echo_="    Warning: no or too old $name found,"; echo "$echo_" >> $CONF_LOG; test -n "$ECHO" && $ECHO "$echo_"
+      WARNING=1
+    fi
+    echo_="  need at least version $minversion, download: $url"; echo "$echo_" >> $CONF_LOG; test -n "$ECHO" && $ECHO "$echo_"
+  fi
+fi
+
+if [ -n "$EXIV2" ] && [ $EXIV2 -gt 0 ]; then
+  name="exiv2"
+  libname=$name
+  minversion=0.10
+  url="http://www.exiv2.org/"
+  TESTER=$EXIV2
+  ID=EXIV2
 
   ID_H="$ID"_H
   ID_LIBS="$ID"_LIBS
@@ -467,7 +514,7 @@ if [ -n "$LRAW" ] && [ $LRAW -gt 0 ]; then
   fi
   if [ $HAVE_LIB -eq 1 ]; then
     if [ "$version" != "" ]; then
-      echo_="$name $version           detected"; echo "$echo_" >> $CONF_LOG; test -n "$ECHO" && $ECHO "$echo_"
+      echo_="$name $version            detected"; echo "$echo_" >> $CONF_LOG; test -n "$ECHO" && $ECHO "$echo_"
     else
       echo_="lib$name                  detected"; echo "$echo_" >> $CONF_LOG; test -n "$ECHO" && $ECHO "$echo_"
     fi
@@ -481,6 +528,44 @@ if [ -n "$LRAW" ] && [ $LRAW -gt 0 ]; then
     fi
     echo_="  need at least version $minversion, download: $url"; echo "$echo_" >> $CONF_LOG; test -n "$ECHO" && $ECHO "$echo_"
   fi
+fi
+
+if [ -n "$XCM" ] && [ $XCM -gt 0 ]; then
+    found=""
+    version=""
+    pc_package=xcm
+    name="xcm"
+    libname=$name
+    minversion=0.7
+    ID=XCM
+
+    ID_H="$ID"_H
+    ID_LIBS="$ID"_LIBS
+    if [ -z "$found" ]; then
+      pkg-config  --atleast-version=0.2 $pc_package
+      if [ $? = 0 ]; then
+        found=`pkg-config --cflags $pc_package`
+        version=`pkg-config --modversion $pc_package`
+      fi
+    fi
+    if [ -n "$found" ]; then
+      if [ -n "$version" ]; then
+        echo_="X CM $version              detected"; echo "$echo_" >> $CONF_LOG; test -n "$ECHO" && $ECHO "$echo_"
+      else
+        echo_="X CM                    detected"; echo "$echo_" >> $CONF_LOG; test -n "$ECHO" && $ECHO "$echo_"
+      fi
+      echo "#define HAVE_XCM 1" >> $CONF_H
+      if [ -n "$MAKEFILE_DIR" ]; then
+        for i in $MAKEFILE_DIR; do
+          test -f "$ROOT_DIR/$i/makefile".in && echo "XCM = 1" >> "$i/makefile"
+          test -f "$ROOT_DIR/$i/makefile".in && echo "XCM_H = $found" >> "$i/makefile"
+          test -f "$ROOT_DIR/$i/makefile".in && echo "$ID_LIBS = `pkg-config --libs $name | sed \"$STRIPOPT\"`" >> "$i/makefile"
+        done
+      fi
+    elif [ $OSUNAME = "Linux" ]; then
+      echo_="X CM not found in"; echo "$echo_" >> $CONF_LOG; test -n "$ECHO" && $ECHO "$echo_"
+      echo_="  $pc_package.pc"; echo "$echo_" >> $CONF_LOG; test -n "$ECHO" && $ECHO "$echo_"
+    fi
 fi
 
 if [ -n "$X11" ] && [ $X11 -gt 0 ]; then
@@ -511,36 +596,6 @@ if [ -n "$X11" ] && [ $X11 -gt 0 ]; then
     fi
 fi
 if [ -n "$X11" ] && [ $X11 -gt 0 ]; then
-  if [ -n "$XCM" ] && [ $XCM -gt 0 ]; then
-    found=""
-    version=""
-    pc_package=xcm
-    if [ -z "$found" ]; then
-      pkg-config  --atleast-version=0.2 $pc_package
-      if [ $? = 0 ]; then
-        found=`pkg-config --cflags $pc_package`
-        version=`pkg-config --modversion $pc_package`
-      fi
-    fi
-    if [ -n "$found" ]; then
-      if [ -n "$version" ]; then
-        echo_="X CM $version              detected"; echo "$echo_" >> $CONF_LOG; test -n "$ECHO" && $ECHO "$echo_"
-      else
-        echo_="X CM                    detected"; echo "$echo_" >> $CONF_LOG; test -n "$ECHO" && $ECHO "$echo_"
-      fi
-      echo "#define HAVE_XCM 1" >> $CONF_H
-      if [ -n "$MAKEFILE_DIR" ]; then
-        for i in $MAKEFILE_DIR; do
-          test -f "$ROOT_DIR/$i/makefile".in && echo "XCM = 1" >> "$i/makefile"
-          test -f "$ROOT_DIR/$i/makefile".in && echo "XCM_INC = $found" >> "$i/makefile"
-        done
-      fi
-    elif [ $OSUNAME = "Linux" ]; then
-      echo_="X CM not found in"; echo "$echo_" >> $CONF_LOG; test -n "$ECHO" && $ECHO "$echo_"
-      echo_="  $pc_package.pc"; echo "$echo_" >> $CONF_LOG; test -n "$ECHO" && $ECHO "$echo_"
-    fi
-  fi
-
   if [ -n "$XF86VMODE" ] && [ $XF86VMODE -gt 0 ]; then
     found=""
     version=""
@@ -774,7 +829,7 @@ if [ -n "$X11" ] && [ $X11 -gt 0 ]; then
   if [ -n "$MAKEFILE_DIR" ]; then
     for i in $MAKEFILE_DIR; do
       test -f "$ROOT_DIR/$i/makefile".in && echo "X11_INCL=\$(XF86VMODE_INC) \$(XINERAMA_INC) \$(XRANDR_INC)" >> "$i/makefile"
-      test -f "$ROOT_DIR/$i/makefile".in && echo "X11_LIBS=\$(X11_LIB_PATH) -lX11 $X_ADD_LIBS" >> "$i/makefile"
+      test -f "$ROOT_DIR/$i/makefile".in && echo "X11_LIBS=\$(X11_LIB_PATH) -lX11 $X_ADD_LIBS \$(XCM_LIBS)" >> "$i/makefile"
     done
   fi
 fi
@@ -940,7 +995,14 @@ if [ -n "$COMPIZ" ] && [ $COMPIZ -gt 0 ]; then
     echo "COMPIZ_H = `pkg-config --cflags $pc_package | sed \"$STRIPOPT\"`" >> $CONF
     echo "COMPIZ_LIBS = `pkg-config --libs $pc_package | sed \"$STRIPOPT\"`" >> $CONF
   else
-    echo_="  no or too old $pc_package found, need $pc_package to build examples"; echo "$echo_" >> $CONF_LOG; test -n "$ECHO" && $ECHO "$echo_"
+    echo__="  no or too old $pc_package found, need $pc_package";
+    if [ $COMPIZ -eq 1 ]; then
+      ERROR=1
+      echo_="!!! ERROR !!! $echo__"; echo "$echo_" >> $CONF_LOG; test -n "$ECHO" && $ECHO "$echo_"
+    else
+      echo_="    Warning   $echo__"; echo "$echo_" >> $CONF_LOG; test -n "$ECHO" && $ECHO "$echo_"
+      WARNING=1
+    fi
   fi
 fi
 
@@ -1060,6 +1122,7 @@ if [ -n "$DEBUG" ] && [ $DEBUG -gt 0 ]; then
       fi
       if [ "$verbose" -eq "0" ]; then
         test -f "$ROOT_DIR/$i/makefile".in && echo ".SILENT:"  >> "$i/makefile"
+        test -f "$ROOT_DIR/$i/makefile".in && echo ""  >> "$i/makefile"
       fi
     done
   fi
