89aacc4400a32b18becf30d540b4820c3f7da92e
Kai-Uwe Behrmann <ku.b@gmx.de>
2013-02-16 21:40:02 +0100

    * [core]: use available XRandR for monitor gamma
    This fixes video card gamma detection on XRandR systems.

--- icc_helfer_x.cpp
+++ icc_helfer_x.cpp
@@ -45,6 +45,9 @@
 #ifdef HAVE_XIN
   #include <X11/extensions/Xinerama.h>
 #endif
+#if defined( HAVE_XRANDR )
+  # include <X11/extensions/Xrandr.h>
+#endif
 #ifdef HAVE_FLTK
 #include <FL/x.H>
 #endif
@@ -165,6 +168,50 @@ leseGrafikKartenGamma        (std::string display_name,
     
   DBG_PROG_V( ScreenCount( display ) )
 
+  static int xrr_version = -1;
+  int gamma_size = 0;
+  int crtc = 0;
+# if defined(HAVE_XRANDR)
+  int major_versionp = 0;
+  int minor_versionp = 0;
+  int i, n = 0;
+  Window root = RootWindow(display, DefaultScreen( display )); 
+
+  if(xrr_version == -1)
+  {
+    XRRQueryVersion( display, &major_versionp, &minor_versionp );
+    xrr_version = major_versionp*100 + minor_versionp;
+  }
+
+  if(xrr_version >= 102)
+  {                           
+    static XRRScreenResources * res = 0;
+    if(!res)
+      res = XRRGetScreenResources( display, root );
+
+    if(res && res->ncrtc == n_xin_info)
+    {
+      int ncrtc = 0;
+      n = res->noutput;
+      for( i = 0; i < n; ++i )
+      {
+        RROutput output = res->outputs[i];
+        XRROutputInfo * output_info = XRRGetOutputInfo( display, res,
+                                                        output);
+        if(output_info->crtc && ncrtc++ == screen)
+        {
+          crtc = output_info->crtc;
+          gamma_size = XRRGetCrtcGammaSize( display, output_info->crtc );
+        }
+
+        XRRFreeOutputInfo( output_info ); output_info = 0;
+      }
+    }
+    //XRRFreeScreenResources(res); res = 0;
+  } 
+    
+#endif /* HAVE_XRANDR */
+
   XF86VidModeGamma gamma;
   XF86VidModeMonitor monitor;
   //int screen = DefaultScreen( display );
@@ -188,6 +235,14 @@ leseGrafikKartenGamma        (std::string display_name,
     }
   DBG_PROG_V( DisplayWidth(display, screen) <<" "<< DisplayWidthMM(display, screen) )
 
+  if(xrr_version >= 102 && gamma_size)
+  {
+    char t[24];
+    texte.push_back("");
+    texte.push_back(_("Gamma:   "));
+    sprintf(t, "%d", gamma_size);
+    texte[texte.size()-1].append(t);
+  } else
   if (!XF86VidModeGetGamma(display, effective_screen, &gamma))
   { DBG_PROG_S( "no gamma information obtained" );
   } else {
@@ -218,6 +273,35 @@ leseGrafikKartenGamma        (std::string display_name,
     DBG_PROG_S( "no gammagradient information obtained" );
 
   DBG_PROG_V( size )
+# if defined(HAVE_XRANDR)
+  if(xrr_version >= 102 && gamma_size)
+  {
+    size = gamma_size;
+    unsigned short *red   = new unsigned short [size],
+                   *green = new unsigned short [size],
+                   *blue  = new unsigned short [size];
+
+    XRRCrtcGamma * gamma = XRRGetCrtcGamma(display, crtc);
+
+    if(!gamma)
+      WARN_S( _("no gammagradient information obtained") )
+
+    kurven.resize(3);
+    for( int i = 0; i < 3; ++i) {
+      kurven[i].resize(size);
+    }
+    for(int j = 0; j < size; ++j) {
+        kurven[0][j] = gamma->red[j]  /65535.0;
+        kurven[1][j] = gamma->green[j]/65535.0;
+        kurven[2][j] = gamma->blue[j] /65535.0;
+    }
+
+    XRRFreeGamma (gamma);
+    delete [] red;
+    delete [] green;
+    delete [] blue;
+  } else
+#endif
   if (size)
   {
     unsigned short *red   = new unsigned short [size],
